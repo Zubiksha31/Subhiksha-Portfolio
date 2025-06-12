aed07af586c565c6e02126611b62be716871cab7
Subhiksha S <subhiksha.senthilkumarr@gmail.com>
2025-06-12 20:09:55 +0530

    changes

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
       <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4">View My Resume</h1>
       <a
-        href="https://drive.google.com/file/d/1mOc0QrTIS8FphsQxdIn0yDydH6Gxy-A9/view?usp=drive_link"
+        href="https://drive.google.com/file/d/148ywPxGSu9G9F042Kgr65xON2rzDj0kW/view?usp=drive_link"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
